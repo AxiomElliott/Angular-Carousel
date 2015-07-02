b5d78879f5cd74ea67cbd1de9cd936f296f381c1
Elliott Thompson <ellth92@gmail.com>
2015-07-02 13:31:26 +0100

    added new files to the repo

--- carousel.js
+++ carousel.js
@@ -1,5 +1,27 @@
 var carouselAng = angular.module('carouselAng', []);
 
-carouselAng.controller('mainController', $scope, function($scope) {
-	console.log($scope);
+carouselAng.controller('cController', function($scope){
+	$scope.navigation = (function(){
+		return json.NavArray;
+	})();
 });
+
+var json = {
+	"NavArray":[
+		{
+			"Navigation":"Nintendo", 
+			"URL":"/nintendo/",
+			"SubNavigation":[{"Name":"Gameboy", "URL":""},{"Name":"Gamecube", "URL":""},{"Name":"N64", "URL":""}]
+		},
+		{
+			"Navigation":"Sony", 
+			"URL":"/sony/",
+			"SubNavigation":[{"Name":"PSP", "URL":""},{"Name":"PS2", "URL":""},{"Name":"Vita", "URL":""}]
+		},
+		{
+			"Navigation":"Microsoft", 
+			"URL":"/microsoft/",
+			"SubNavigation":[{"Name":"PC", "URL":""},{"Name":"Xbox 360", "URL":""},{"Name":"Xbox One", "URL":""}]
+		}
+	]
+};
