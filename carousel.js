d0a6922a1f399a3f131183763a3c3c0a31cc593c
Elliott Thompson <ellth92@gmail.com>
2015-07-04 01:42:44 +0100

    Created angular logic for the carousel rotation, applies some css to begin styling the view

--- carousel.js
+++ carousel.js
@@ -2,23 +2,40 @@ var carouselAng = angular.module('carouselAng', []);
 
 carouselAng.controller('cController', ['$scope', '$interval', function($scope, $interval){
 	$scope.carouselPercentage = 0;
-
+	$scope.selectedIndex = 0;
 	$scope.manufacturers = (function(){
 		return json;
 	})();
 
+	$scope.checkShow = function(index) {
+		return (index === $scope.selectedIndex);
+	};
+
 	$scope.loadingBarWidth = function () {
 		return { width:($scope.carouselPercentage / 20) + '%' };
 	};
+	$scope.setSelectedIndex = function(index) {
+		$scope.selectedIndex = index;
+		$scope.carouselPercentage = 0;
+	}
+
+	$scope.getNavSelected = function(index) {
+		return (index === $scope.selectedIndex) ? "navigationitemselected" : "navigationitem";
+	}
 
 	function stepPercentage() {
 		$scope.carouselPercentage += 1;
 		if ($scope.carouselPercentage > 2000){
 			$scope.carouselPercentage = 0;
+			moveCarousel();
 		}
 	}
 
-	var timer = $interval(stepPercentage, 10);
+	function moveCarousel() {
+		$scope.selectedIndex  = $scope.manufacturers.length === ($scope.selectedIndex + 1)? 0 : $scope.selectedIndex + 1;
+	}
+
+	var timer = $interval(stepPercentage, 2.5);
 	
 }]);
 
